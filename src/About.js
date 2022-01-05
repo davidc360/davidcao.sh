a84acb208dbc364809d781601ce87657ea10e30b
David Cao <davidcao@Davids-MBP-16.fios-router.home>
2022-01-05 04:30:09 -0500

    first commit

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div className="aboutCtn">
+            <div className="heading">About</div> 
+        </div>
+    )
+}
---
+++ src/Greeting.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Greeting.sass'
+
+export default function Greeting() {
+    return (
+        <div className="greetingCtn">
+            <div className="introText">Hello, I'm David Cao!</div>
+        </div>
+    )
+}
