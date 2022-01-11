67a3d1ca18b602e2097b6a00e6164e5f175c70e1
David Cao <davidcao@davids-mbp-16.myfiosgateway.com>
2022-01-11 01:41:12 -0500

    added frames around screenshot

--- src/Projects.js
+++ src/Projects.js
@@ -11,13 +11,19 @@ export default function Projects() {
             <Project
                 title="FastKeys.io"
                 description="FastKeys.io is a full-stack type-racing web game that allows players to challenge other players on typing speed and accuracy."
+                link="https://fastkeys.io"
                 images={[
-                    {
-                        original: light,
-                    },
-                    {
-                        original: light2,
-                    }
+                    { original: light },
+                    { original: light2 }
+                ]}
+            />
+            <Project
+                title="Popular Tickers"
+                description="Popular Tickers is a full-stack web application that monitors retail trading interest in the stock market based on sentiment on social media platform Reddit."
+                link="https://populartickers.com"
+                images={[
+                    { original: light },
+                    { original: light2 }
                 ]}
             />
         </div>
@@ -27,15 +33,27 @@ export default function Projects() {
 function Project({ title, description, link, images }) {
     return (
         <div className="projectCtn">
-            <div className="projectTitle">{title}</div>
-            <p className="projectDesc">{description}</p>
-            <ImageGallery
-                items={images}
-                showFullscreenButton={false}
-                showPlayButton={false}
-                autoPlay={true}
-                showThumbnails={false}
-            />
+            <div className="projectLeft">
+                <span className="projectTitle">{title}</span>
+                <span className="titleDivider">|</span>
+                <a className="link" href={link}>{link}</a>
+                <p className="projectDescription">{description}</p>
+            </div>
+            <div className="projectRight">
+                <div className="projectGallery">
+                    <div className="firstBorder">
+                        <ImageGallery
+                            items={images}
+                            showFullscreenButton={false}
+                            showPlayButton={false}
+                            autoPlay={true}
+                            showThumbnails={false}
+                            showNav={false}
+                            />
+                        <div className="secondBorder"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
