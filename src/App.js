ebe2cfdf10b0b4c333cfa6efd7055d0edf2e148e
David Cao <davidcao@Davids-MBP-16.fios-router.home>
2022-01-15 04:10:32 -0500

    updated redirect mechanism

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import Contact from './Contact'
 import "react-image-gallery/styles/css/image-gallery.css";
 
 function App() {
-    const path = window.location.pathname
-    if (path === '/slash-to-search/') {
-        window.open('https://chrome.google.com/webstore/detail/slash-to-search/ijckekobipioaegifkgdllplckmaojdj', '_self')
-    }
     return (
         <div className="App">
             <Greeting />
