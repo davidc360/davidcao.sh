f8499a44404c0806e55270febb23299da32919a7
David Cao <davidcao@Davids-MBP-16.fios-router.home>
2022-01-17 06:14:06 -0500

    added res

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import About from './About'
 import Projects from './Projects'
 import Contact from './Contact'
 import "react-image-gallery/styles/css/image-gallery.css";
+import resume from './resume.pdf'
 
 function App() {
     return (
         <div className="App">
-            <Greeting />
-            <About />
-            <Projects />
-            <Contact />
-            {/* footer */}
-            <div className="footer">
-                Designed and developed by David Cao ❤️
-            </div>
+            {window.location.pathname === '/resume/' ? 
+                <>
+                    <div className="resumeHeader">
+                        <a className="resumeHeaderLink button" href="/">Homepage</a>
+                        <a className="resumeHeaderLink button" href={resume} download={true}>Download PDF</a>
+                    </div>
+                    <embed src={resume} target="_blank" type="application/pdf" style={{ width: "100vw", height: "100vh" }}/>
+                </>
+                :
+                <>
+                    <Greeting />
+                    <About />
+                    <Projects />
+                    <Contact />
+                    {/* footer */}
+                    <div className="footer">
+                        Designed and developed by David Cao ❤️
+                    </div>
+                </>
+            }
         </div>
   )
 }
